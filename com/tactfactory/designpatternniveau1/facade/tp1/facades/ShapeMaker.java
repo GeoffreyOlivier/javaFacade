86fa74aa6eac67d08ffed7a0e11f30fc3960b509
Faune_GO <geoffrey.olivier@faune.net>
2020-11-10 15:24:49 +0100

    essai tp sur les facades

---
+++ com/tactfactory/designpatternniveau1/facade/tp1/facades/ShapeMaker.java
@@ -0,0 +1,26 @@
+package com.tactfactory.designpatternniveau1.facade.tp1.facades;
+import java.util.Arrays;
+import java.util.List;
+public class ShapeMaker {
+
+    List<ShapeMaker> shapeMakers = Arrays.asList(new Circle(), new Rectangle(), new Square());
+//
+//    for (ShapeMaker facade : shapeMakers) {
+//        if( ShapeMaker instanceof circle){
+//
+//        }
+//    }
+
+    public void drawCircle(){
+        rectangle.draw  //ceci ne fonctionne pas il faut le mettre dans un objet
+    }
+    public void drawRectangle(){
+
+    }
+    public void drawSquare(){
+
+    }
+
+//    list de shape
+
+}
---
+++ com/tactfactory/designpatternniveau1/facade/tp1/facades/Square.java
@@ -0,0 +1,8 @@
+package com.tactfactory.designpatternniveau1.facade.tp1.facades;
+
+public class Square extends ShapeMaker {
+
+    public void draw(){
+        System.out.println("draw Square");
+    }
+}
